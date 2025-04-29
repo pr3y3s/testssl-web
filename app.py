931013b4d2933a60ebc63450878a53ba7ef10369
Pablo Reyes <167684902+pr3y3s@users.noreply.github.com>
2025-04-30 00:12:23 +0200

    Update app.py - Bad indent

--- app.py
+++ app.py
@@ -57,16 +57,16 @@ def execute_testssl(url, option):
         url (str): The URL of the website to analyze.
         option (str): The analysis option.
     """
-        # Comando testssl
-        if option in ['stic221', 'stic807']:
-            # Ejecutamos el comando testssl y redirigimos la salida al archivo temporal
-            subprocess.run(
-                ["testssl", "--csv", "--mapping", "iana", "-E", "-f", url],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL
-            )
-        else:
-            subprocess.run(f"testssl --csv -S {url} > 2>&1", shell=True)
+    # Comando testssl
+    if option in ['stic221', 'stic807']:
+        # Ejecutamos el comando testssl y redirigimos la salida al archivo temporal
+        subprocess.run(
+            ["testssl", "--csv", "--mapping", "iana", "-E", "-f", url],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+    else:
+        subprocess.run(f"testssl --csv -S {url} > 2>&1", shell=True)
 
 def find_file(url):
     """
